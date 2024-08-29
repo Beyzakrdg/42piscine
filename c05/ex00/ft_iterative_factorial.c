7f36c7166acbfa0d3ee4ff5a2dcb3c9d1265e8aa
Beyza Karadağ <146764175+Beyzakrdg@users.noreply.github.com>
2024-08-29 20:50:23 +0300

    Add files via upload

---
+++ c05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bekarada <bekarada@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 11:04:17 by bekarada          #+#    #+#             */
+/*   Updated: 2024/08/25 11:28:43 by bekarada         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	unsigned int	result;
+
+	result = 1;
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	while (nb > 0)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bekarada <bekarada@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 11:51:09 by bekarada          #+#    #+#             */
+/*   Updated: 2024/08/25 12:12:51 by bekarada         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power == 0)
+		return (1);
+	else if (power < 0)
+		return (0);
+	else
+		return (nb * ft_recursive_power(nb, (power - 1)));
+}
