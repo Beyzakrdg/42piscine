710cabce83a865cae8188f667b439334bb458502
Beyza Karadağ <146764175+Beyzakrdg@users.noreply.github.com>
2024-09-09 19:54:03 +0300

    Add files via upload

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,20 @@
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (src[i])
+		{
+			dest[i] = src[i];
+		}
+		else
+		{
+			dest[i] = '\0';
+		}
+		i++;
+	}
+	return (dest);
+}
